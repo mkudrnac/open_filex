6f7de9e1d249850bd89808161f721c8c3cd9a8ce
Miroslav Kudrnac <12230741+mkudrnac@users.noreply.github.com>
2023-02-09 15:13:24 +0100

    Update OpenFilePlugin.m

--- ios/Classes/OpenFilePlugin.m
+++ ios/Classes/OpenFilePlugin.m
@@ -7,76 +7,53 @@ static NSString *const CHANNEL_NAME = @"open_file";
 
 @implementation OpenFilePlugin{
     FlutterResult _result;
-    UIViewController *_viewController;
-    UIDocumentInteractionController *_documentController;
-    UIDocumentInteractionController *_interactionController;
 }
 
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
     FlutterMethodChannel* channel = [FlutterMethodChannel
                                      methodChannelWithName:CHANNEL_NAME
                                      binaryMessenger:[registrar messenger]];
-    OpenFilePlugin* instance = [[OpenFilePlugin alloc] initWithViewController:nil];
+    OpenFilePlugin* instance = [[OpenFilePlugin alloc] init];
     [registrar addMethodCallDelegate:instance channel:channel];
-}
-
-+ (UIViewController*) topMostController
-{
-    UIViewController *topController = [UIApplication sharedApplication].keyWindow.rootViewController;
-
-    while (topController.presentedViewController) {
-        topController = topController.presentedViewController;
-    }
-
-    return topController;
-}
-
-- (instancetype)initWithViewController:(UIViewController *)viewController {
-    self = [super init];
-    if (self) {
-        _viewController = viewController;
-    }
-    return self;
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
     if ([@"open_file" isEqualToString:call.method]) {
         _result = result;
         NSString *msg = call.arguments[@"file_path"];
-        if(msg==nil){
+        if (msg == nil){
             NSDictionary * dict = @{@"message":@"the file path cannot be null", @"type":@-4};
             NSData * jsonData = [NSJSONSerialization dataWithJSONObject:dict options:NSJSONWritingPrettyPrinted error:nil];
             NSString * json = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
             result(json);
             return;
         }
-        NSFileManager *fileManager=[NSFileManager defaultManager];
-        BOOL fileExist=[fileManager fileExistsAtPath:msg];
-        if(fileExist){
-            _documentController = [UIDocumentInteractionController interactionControllerWithURL:[NSURL fileURLWithPath:msg]];
-            _documentController.delegate = self;
+        NSFileManager *fileManager = [NSFileManager defaultManager];
+        BOOL fileExist = [fileManager fileExistsAtPath:msg];
+        if (fileExist) {
+            UIDocumentInteractionController* documentController = [UIDocumentInteractionController interactionControllerWithURL:[NSURL fileURLWithPath:msg]];
+            documentController.delegate = self;
             NSString *uti = call.arguments[@"uti"];
             BOOL isBlank = [self isBlankString:uti];
-            if(!isBlank){
-                _documentController.UTI = uti;
+            if (!isBlank) {
+                documentController.UTI = uti;
             }
             
             @try {
-                BOOL previewSucceeded = [_documentController presentPreviewAnimated:YES];
-                if(!previewSucceeded){
-                    [_documentController presentOpenInMenuFromRect:CGRectMake(500,20,100,100) inView:[OpenFilePlugin topMostController].view animated:YES];
+                BOOL previewSucceeded = [documentController presentPreviewAnimated:YES];
+                if (!previewSucceeded) {
+                    [documentController presentOpenInMenuFromRect:CGRectMake(500, 20, 100, 100) inView:[self topMostController].view animated:YES];
                 }
-            }@catch (NSException *exception) {
+            } @catch (NSException *exception) {
                 NSDictionary * dict = @{@"message":@"File opened incorrectly。", @"type":@-4};
                 NSData * jsonData = [NSJSONSerialization dataWithJSONObject:dict options:NSJSONWritingPrettyPrinted error:nil];
                 NSString * json = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
                 result(json);
             }
-        }else{
+        } else {
             NSDictionary * dict = @{@"message":@"the file does not exist", @"type":@-2};
             NSData * jsonData = [NSJSONSerialization dataWithJSONObject:dict options:NSJSONWritingPrettyPrinted error:nil];
             NSString * json = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-            
             result(json);
         }
     } else {
@@ -88,7 +65,6 @@ static NSString *const CHANNEL_NAME = @"open_file";
     NSDictionary * dict = @{@"message":@"done", @"type":@0};
     NSData * jsonData = [NSJSONSerialization dataWithJSONObject:dict options:NSJSONWritingPrettyPrinted error:nil];
     NSString * json = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-    
     _result(json);
 }
 
@@ -96,12 +72,19 @@ static NSString *const CHANNEL_NAME = @"open_file";
     NSDictionary * dict = @{@"message":@"done", @"type":@0};
     NSData * jsonData = [NSJSONSerialization dataWithJSONObject:dict options:NSJSONWritingPrettyPrinted error:nil];
     NSString * json = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-    
     _result(json);
 }
 
 - (UIViewController *)documentInteractionControllerViewControllerForPreview:(UIDocumentInteractionController *)controller {
-    return [OpenFilePlugin topMostController];
+    return [self topMostController];
+}
+
+- (UIViewController*) topMostController{
+    UIViewController *topController = [UIApplication sharedApplication].keyWindow.rootViewController;
+    while (topController.presentedViewController) {
+        topController = topController.presentedViewController;
+    }
+    return topController;
 }
 
 - (BOOL) isBlankString:(NSString *)string {
@@ -111,7 +94,7 @@ static NSString *const CHANNEL_NAME = @"open_file";
     if ([string isKindOfClass:[NSNull class]]) {
         return YES;
     }
-    if ([[string stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]] length]==0){
+    if ([[string stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]] length] == 0) {
         return YES;
     }
     return NO;
